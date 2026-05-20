e9313179f173ecb5a83dd850faed5290270b1557
Ziko <62886883+zakariahxmda@users.noreply.github.com>
2026-05-20 18:58:15 -0400

    Add complete web booking flow with Ihjaz branding

--- frontend/BookingApp/web-server.js
+++ frontend/BookingApp/web-server.js
@@ -21,6 +21,26 @@ app.get('/home.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
+app.get('/profile.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'profile.html'));
+});
+
+app.get('/service-selection.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'service-selection.html'));
+});
+
+app.get('/datetime-selection.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'datetime-selection.html'));
+});
+
+app.get('/booking-confirmation.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'booking-confirmation.html'));
+});
+
+app.get('/bookings-list.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'bookings-list.html'));
+});
+
 app.listen(port, () => {
   console.log(`Web server running at http://localhost:${port}`);
 });
